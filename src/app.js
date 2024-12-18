0f94bc1f07cf5a31c0fafaaf8d4f8723707caebb
Manikandan-K-S <Manikandan-K-S@users.noreply.github.com>
2024-12-18 12:58:56 +0530

    12:58

--- src/app.js
+++ src/app.js
@@ -80,7 +80,11 @@ app.get('/staff/achievement/:id', preventCache, checkStaffSession, fetchAchievem
 
 app.get('/events', preventCache, checkStaffSession, (req, res) => res.render('event'));
 
-app.get('/api/events', preventCache, checkStaffSession, getEvents);
+app.get('/api/events', preventCache, getEvents);
+
+app.get("/tutorAccess",(req,res)=>{
+  res.redirect("/staff/profile");
+})
 
 app.delete('/api/events/:id', preventCache,checkStaffSession, async (req, res) => {
   try {
