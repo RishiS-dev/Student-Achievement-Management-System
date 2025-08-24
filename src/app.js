da28e8c6f028f91307a24918943f34f2a88eeecd
Rishi Sundaresan <rishi200322@gmail.com>
2025-08-24 19:00:24 +0530

    production update 2

--- src/app.js
+++ src/app.js
@@ -27,8 +27,7 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.set("view engine", "ejs");
-
+app.set("views", path.join(__dirname, "../views"));
 app.use(express.static(path.join(__dirname, "../public")));
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 
